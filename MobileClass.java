2abc0a67ecc90e8f8aa2cf279385b266b2174011
IndhumathiCK <124440939+IndhumathiCK@users.noreply.github.com>
2023-02-04 19:37:23 +0530

    Add files via upload
    Add week 1 home work

---
+++ MobileClass.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class MobileClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String mobileBrandName="OnePlus";
+		char mobileLogo='1';
+		short noOfMobilePiece= 1;
+		int modelNumber= 10;
+		long mobileImeiNumber= 3456758436561L;
+		float mobilePrice= 380000f;
+		boolean isDamaged= false;
+		
+		System.out.println("Mobile Brand Name:" +mobileBrandName);
+		System.out.println("Mobile Logo:" +mobileLogo);
+		System.out.println("No Of MobilePiece:" +noOfMobilePiece);
+		System.out.println("Model Number:" +modelNumber);
+		System.out.println("Mobile Imei Number:" +mobileImeiNumber);
+		System.out.println("Mobile Price:" +mobilePrice);
+		System.out.println("Damaged:" +isDamaged);
+	}
+
+}
---
+++ PrintDataTypes.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class PrintDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a=1;
+		short c= -200;
+		int i = 500;
+		long d = 10000000000L;
+		float f= 11000000.34566f;
+		double g= 1114534363.436454576;
+		boolean j= true;
+		char h='T';
+		String b="Sample";
+		System.out.println(i);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(h);
+		System.out.println(j);
+	}
+
+}
